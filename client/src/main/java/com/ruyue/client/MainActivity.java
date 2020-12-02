82e06aae832cb2dd1a3959762964338061feb819
ruyue-zhang <772346730@qq.com>
2020-12-02 21:29:23 +0800

    [ruyue] feat:add service connection

--- client/src/main/java/com/ruyue/client/MainActivity.java
+++ client/src/main/java/com/ruyue/client/MainActivity.java
@@ -2,15 +2,47 @@ package com.ruyue.client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+
+import com.ruyue.goods.IRemoteService;
 
 public class MainActivity extends AppCompatActivity {
+    private IRemoteService iRemoteService;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            iRemoteService = IRemoteService.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            iRemoteService = null;
+        }
+    };
+
+    private void bindService() {
+        Intent intent = new Intent("com.ruyue.goods.MyService");
+        intent.setPackage("com.ruyue.goods");
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindService();
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(connection);
+    }
 }
