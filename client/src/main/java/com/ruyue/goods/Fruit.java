4edecdbe8d98cad6847accf8bcbd7ec0b393edd7
ruyue-zhang <772346730@qq.com>
2020-12-02 18:08:31 +0800

    [ruyue] feat:add Fruit class in client

---
+++ client/src/main/java/com/ruyue/goods/Fruit.java
@@ -0,0 +1,50 @@
+package com.ruyue.goods;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Fruit implements Parcelable {
+    private String imgLink;
+    private String name;
+    private String description;
+    private double price;
+
+    public Fruit(String imgLink, String name, String description, double price) {
+        this.imgLink = imgLink;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    protected Fruit(Parcel in) {
+        imgLink = in.readString();
+        name = in.readString();
+        description = in.readString();
+        price = in.readDouble();
+    }
+
+    public static final Creator<Fruit> CREATOR = new Creator<Fruit>() {
+        @Override
+        public Fruit createFromParcel(Parcel in) {
+            return new Fruit(in);
+        }
+
+        @Override
+        public Fruit[] newArray(int size) {
+            return new Fruit[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(imgLink);
+        dest.writeString(name);
+        dest.writeString(description);
+        dest.writeDouble(price);
+    }
+}
